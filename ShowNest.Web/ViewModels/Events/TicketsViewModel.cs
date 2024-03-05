7d1c8dbda0d0276e89943b8b7de2eb6f2982e04d
PJ Shi <michelle760994@gmail.com>
2024-03-06 05:23:05 +0800

    替EventPage 建立ViewModel並塞了部分資料進去

--- ShowNest.Web/ViewModels/Events/TicketsViewModel.cs
+++ ShowNest.Web/ViewModels/Events/TicketsViewModel.cs
@@ -7,5 +7,9 @@ namespace ShowNest.Web.ViewModels.Events
         public string TicketTypeName { get; set; }
         public decimal TicketPrice { get; set; }
         public int  PurchaseAmount { get; set; }
+
+        public DateTime TicketSalseBegin { get; set; }
+        public DateTime TicketSalseEnd { get; set; }
+    public List<TicketsViewModel> AllTickets { get; set; }
     }
 }
