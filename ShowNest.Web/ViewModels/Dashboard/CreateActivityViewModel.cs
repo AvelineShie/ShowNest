6c960334f9aed821f6d2c5012fbef8a2504d4fba
Aveline <evelyne1122@gmail.com>
2024-03-06 08:13:03 +0800

    add OrgID

--- ShowNest.Web/ViewModels/Dashboard/CreateActivityViewModel.cs
+++ ShowNest.Web/ViewModels/Dashboard/CreateActivityViewModel.cs
@@ -5,6 +5,7 @@ namespace ShowNest.Web.ViewModels.Dashboard
 {
     public class CreateActivityViewModel
     {
+        public int OrganizationId { get; set; }
         public string OrganizationName { get; set; }
         public EventDetailViewModel EventName { get; set; }
     }
