5f9a8291766c34008753b2b50b1135f0d497f711
Dani <1073B023@gmail.com>
2024-03-08 20:41:32 +0800

    [Backlog 674] 修改RegistrationViewModel [TASK 677]

--- ShowNest.Web/ViewModels/Events/RegistrationViewModel.cs
+++ ShowNest.Web/ViewModels/Events/RegistrationViewModel.cs
@@ -1,15 +1,40 @@
 ﻿using ShowNest.Web.ViewModels.UserAccount;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShowNest.Web.ViewModels.Events
 {
     public class RegistrationViewModel
     {
-        public EventDetailViewModel EventDetail { get; set; }
-        public TicketsViewModel Tickets { get; set; }
-        public SeatsViewModel Seat { get; set; }
-        public PrefillsInfoViewModel Prefills { get; set; }
+        // Event Information
+        public string MainImage { get; set; }
+        public string EventName { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime StartTime { get; set; }
+        public string EventId { get; set; }
+        public string EventLocation { get; set; }
+        public string EventAddress { get; set; }
+        public string EventHost { get; set; }
+        public string TicketCollectionChannel { get; set; }
+        public string PaymentMethodName { get; set; }
 
+        // Ticket Information
+        public string SeatArea { get; set; }
+        public string SeatRow { get; set; }
+        public int SeatNumber { get; set; }
+        public string TicketTypeName { get; set; }
+        public decimal TicketPrice { get; set; }
+        public int PurchaseAmount { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // Attendee Information
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        // Additional Information
         public bool ShownParticipatedCampaign { get; set; }
 
     }
+ 
+
 }
