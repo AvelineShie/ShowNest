d867186b6d0ba37610793415b3d7f324f7657121
Aveline <evelyne1122@gmail.com>
2024-03-10 16:25:00 +0800

    變更SetActivityViewModel

--- ShowNest.Web/ViewModels/Dashboard/SetActivityViewModel.cs
+++ ShowNest.Web/ViewModels/Dashboard/SetActivityViewModel.cs
@@ -9,15 +9,16 @@ namespace ShowNest.Web.ViewModels.Dashboard
     {
         public int EventId { get; set; }
         public string MainImage { get; set; }
-        public string EventName { get; set;}
-        public bool PrivacyStatus { get; set; }
-        public string WebsiteLink {  get; set; }
+        public string EventName { get; set; }
+        public bool PrivacyStatus { get; set; } //隱私狀態
+        public int EventType { get; set; }//活動分類
+        public string WebsiteLink { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        
-        public int EventAttendance { get; set; } //活動人數
 
-        public int LocationId { get; set; }//場所id
+        public int Attendance { get; set; } //活動人數
+        public int EventStatus {  get; set; }//活動形式
+        public string StreamingName { get; set; }//串流平台名稱
         public string LocationName { get; set; }//場所名稱
         public string EventAddress { get; set; } //活動地址
         public string EventIntroduction {  get; set; }//活動簡介
