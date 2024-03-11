26c1d96827ae86ecf00a6364c1ed1a63076fbe55
AcerYen <aceryen@gmail.com>
2024-03-11 22:06:49 +0800

    Change xxId to Id.

--- ShowNest.Web/Data/DataBaseContext.DataFilled.cs
+++ ShowNest.Web/Data/DataBaseContext.DataFilled.cs
@@ -5,16 +5,16 @@
         private void DataPrefilling(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Area>().HasData(
-             new Area { AreaId = 1, AreaName = "北北基宜地區" },
-             new Area { AreaId = 2, AreaName = "桃竹苗地區" },
-             new Area { AreaId = 3, AreaName = "雲嘉南地區" },
-             new Area { AreaId = 4, AreaName = "高屏地區" },
-             new Area { AreaId = 5, AreaName = "中彰投地區" },
-             new Area { AreaId = 6, AreaName = "花東地區" },
-             new Area { AreaId = 7, AreaName = "澎金馬地區" },
-             new Area { AreaId = 8, AreaName = "香港" },
-             new Area { AreaId = 9, AreaName = "澳門" },
-             new Area { AreaId = 10, AreaName = "其他地區" }
+             new Area { Id = 1, AreaName = "北北基宜地區" },
+             new Area { Id = 2, AreaName = "桃竹苗地區" },
+             new Area { Id = 3, AreaName = "雲嘉南地區" },
+             new Area { Id = 4, AreaName = "高屏地區" },
+             new Area { Id = 5, AreaName = "中彰投地區" },
+             new Area { Id = 6, AreaName = "花東地區" },
+             new Area { Id = 7, AreaName = "澎金馬地區" },
+             new Area { Id = 8, AreaName = "香港" },
+             new Area { Id = 9, AreaName = "澳門" },
+             new Area { Id = 10, AreaName = "其他地區" }
              );
         }
     }
