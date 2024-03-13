f699d36596a412112c97d45100227bb3766c4ed0
Dani <1073B023@gmail.com>
2024-03-13 19:23:11 +0800

    facebook login update

--- ShowNest.Web/Controllers/AccountController.cs
+++ ShowNest.Web/Controllers/AccountController.cs
@@ -4,32 +4,34 @@ namespace ShowNest.Web.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
+        //private readonly ILogger<AccountController> _logger;
 
-        public AccountController(ILogger<AccountController> logger)
-        {
-            _logger = logger;
-        }
+        //public AccountController(ILogger<AccountController> logger)
+        //{
+        //    _logger = logger;
+        //}
         public IActionResult UserEdit()
         {
             var fb = new FacebookClient();
             var loginUrl = fb.GetLoginUrl(new
             {
-                client_id = "1561972534652114", //your_app_id
-                redirect_uri = "https://localhost:7156/Home/FacebookRedirect", //your_redirect_uri
+                client_id = "1346987129576805", //your_app_id
+                redirect_uri = "https://localhost:7156", //your_redirect_uri
                 scope = "public_profile,email"  //public_profile,email
             });
             ViewBag.Url = loginUrl;
             return View();
         }
         public IActionResult FacebookRedirect(string code)
+
         {
             var fb = new FacebookClient();
             dynamic result = fb.Get("/oauth/access_token", new
             {
-                client_id = "1561972534652114", //your_app_id
-                client_secret = "a1c6c2dfc077891ed4c1ef27bc9f8079",
-                redirect_uri = "https://localhost:7156/Home/FacebookRedirect", //your_redirect_uri
+                client_id = "1346987129576805", //your_app_id
+                client_secret = "a647392a10dd9c64d0153eb5eba5eae5",
+                redirect_uri = "https://localhost:7156", //your_redirect_uri
+       
                 code = code
 
             });
