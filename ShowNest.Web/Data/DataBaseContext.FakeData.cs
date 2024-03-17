0f6b3d9f249c030629509a2f0253f8144623a3f1
AcerYen <aceryen@gmail.com>
2024-03-17 15:40:11 +0800

    Clear DBContext to prepare to test.

--- ShowNest.Web/Data/DataBaseContext.FakeData.cs
+++ ShowNest.Web/Data/DataBaseContext.FakeData.cs
@@ -1,27 +1,27 @@
-﻿namespace ShowNest.Web.Data
-{
-    public partial class DataBaseContext
-    {
-        /// <summary>
-        /// Fake data filling
-        /// For testing
-        /// </summary>
-        /// <param name="modelBuilder"></param>
-        private void FakeDataFilling(ModelBuilder modelBuilder)
-        {
-            ///example
-            //modelBuilder.Entity<Area>().HasData(
-            // new Area { Id = 1, AreaName = "北北基宜地區" },
-            // new Area { Id = 2, AreaName = "桃竹苗地區" },
-            // new Area { Id = 3, AreaName = "雲嘉南地區" },
-            // new Area { Id = 4, AreaName = "高屏地區" },
-            // new Area { Id = 5, AreaName = "中彰投地區" },
-            // new Area { Id = 6, AreaName = "花東地區" },
-            // new Area { Id = 7, AreaName = "澎金馬地區" },
-            // new Area { Id = 8, AreaName = "香港" },
-            // new Area { Id = 9, AreaName = "澳門" },
-            // new Area { Id = 10, AreaName = "其他地區" }
-            // );
-        }
-    }
-}
+﻿//namespace ShowNest.Web.Data
+//{
+//    public partial class DataBaseContext
+//    {
+//        /// <summary>
+//        /// Fake data filling
+//        /// For testing
+//        /// </summary>
+//        /// <param name="modelBuilder"></param>
+//        private void FakeDataFilling(ModelBuilder modelBuilder)
+//        {
+//            ///example
+//            //modelBuilder.Entity<Area>().HasData(
+//            // new Area { Id = 1, AreaName = "北北基宜地區" },
+//            // new Area { Id = 2, AreaName = "桃竹苗地區" },
+//            // new Area { Id = 3, AreaName = "雲嘉南地區" },
+//            // new Area { Id = 4, AreaName = "高屏地區" },
+//            // new Area { Id = 5, AreaName = "中彰投地區" },
+//            // new Area { Id = 6, AreaName = "花東地區" },
+//            // new Area { Id = 7, AreaName = "澎金馬地區" },
+//            // new Area { Id = 8, AreaName = "香港" },
+//            // new Area { Id = 9, AreaName = "澳門" },
+//            // new Area { Id = 10, AreaName = "其他地區" }
+//            // );
+//        }
+//    }
+//}
--- ShowNest.Web/Models/PrefillsInfo.cs
+++ ShowNest.Web/Models/PrefillsInfo.cs
@@ -1,30 +1,30 @@
-﻿namespace ShowNest.Web.Models
-{
-    public class PrefillsInfo
-    {
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+﻿//namespace ShowNest.Web.Models
+//{
+//    public class PrefillsInfo
+//    {
+//        [Key]
+//        [Required]
+//        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+//        public int Id { get; set; }
         
-        [Key]
-        [ForeignKey("User")]
-        public int UserId { get; set; }
+//        [Key]
+//        [ForeignKey("User")]
+//        public int UserId { get; set; }
 
-        public string UserName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string County { get; set; }
-        public string District { get; set; }
-        public int? PostalCode { get; set; }
-        public string Address { get; set; }
-        public string OrganiztionName { get; set; }
-        public string Title { get; set; }
-        public int? CompanyZipCode { get; set; }
-        public string CompanyAddress { get; set; }
-        public int? CompanyVAT { get; set; }
-        public int? DonateEInvoice { get; set; }
-        public string MobileEInvoice { get; set; }
+//        public string UserName { get; set; }
+//        public string PhoneNumber { get; set; }
+//        public string County { get; set; }
+//        public string District { get; set; }
+//        public int? PostalCode { get; set; }
+//        public string Address { get; set; }
+//        public string OrganiztionName { get; set; }
+//        public string Title { get; set; }
+//        public int? CompanyZipCode { get; set; }
+//        public string CompanyAddress { get; set; }
+//        public int? CompanyVAT { get; set; }
+//        public int? DonateEInvoice { get; set; }
+//        public string MobileEInvoice { get; set; }
 
-        public virtual User User { get; set; }
-    }
-}
+//        public virtual User User { get; set; }
+//    }
+//}
