e3d614e8d8571379df06e409d2a7261c5e19509e
PJ Shi <michelle760994@gmail.com>
2024-03-20 23:25:57 +0800

    add fakedata

--- Infrastructure/Data/DataBaseContext.FakeData.cs
+++ Infrastructure/Data/DataBaseContext.FakeData.cs
@@ -1,11 +1,14 @@
 ﻿using ApplicationCore.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
 
 namespace Infrastructure.Data
 {
     public partial class DatabaseContext : DbContext
     {
-        void FakeDataFilling(ModelBuilder modelBuilder)
+        static void FakeDataFilling(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(
                 new User
@@ -57,6 +60,85 @@ namespace Infrastructure.Data
                     EditedAt = DateTime.Now.AddDays(-2)
                 }
             );
+
+
+            modelBuilder.Entity<Event>().HasData(
+              new Event
+              {
+                  Id = 1,
+                  OrganizationId = 1,
+                  Name = "【線上直播課】掌握網路三大流量，讓你在同行中脫穎而出",
+                  StartTime = DateTime.Now.AddDays(14),
+                  Type = 0,
+                  StreamingPlatform = "http;",
+                  StreamingUrl = "http;",
+                  Capacity = 500,
+                  ContactPerson = "",
+                  ParticipantPeople = "[\r\n  {\r\n    \"ParticipantPeopleImage\": \"https://picsum.photos/200/200/?random=10\",\r\n    \"ParticipantPeopleId\": \"DDDD\"\r\n  },\r\n  {\r\n    \"ParticipantPeopleImage\": \"https://picsum.photos/200/200/?random=14\",\r\n    \"ParticipantPeopleId\": \"EEEE\"\r\n  },\r\n  {\r\n    \"ParticipantPeopleImage\": \"https://picsum.photos/200/200/?random=18\",\r\n    \"ParticipantPeopleId\": \"FFFF\"\r\n  }\r\n]\r\n",
+                  EventImage = "https://picsum.photos/1300/600/?random=11",
+                  Introduction = "線上活動簡介內容區",
+                  Description = "線上活動描述內容區",
+                  MainOrganizer = "Build School",
+                  IsPrivateEvent = false,
+                  IsFree = true,
+                  Status = 1,
+                  CreatedAt = DateTime.Now,
+                  EditedAt = DateTime.Now
+              },
+              new Event
+              {
+                  Id = 2,
+                  OrganizationId = 2,
+                  Name = "【演唱會】五月天",
+                  StartTime = DateTime.Now,
+                  EndTime = DateTime.Now.AddDays(14),
+                  Type = 1,
+                  LocationName = "大巨蛋",
+                  LocationAddress = "",
+                  Longitude = "120.33333",
+                  Latitude = "120.33333",
+                  Capacity = 1000,
+                  ContactPerson = "",
+                  ParticipantPeople = "[\r\n  {\r\n    \"ParticipantPeopleImage\": \"https://picsum.photos/200/200/?random=10\",\r\n    \"ParticipantPeopleId\": \"DDDD\"\r\n  },\r\n  {\r\n    \"ParticipantPeopleImage\": \"https://picsum.photos/200/200/?random=14\",\r\n    \"ParticipantPeopleId\": \"EEEE\"\r\n  },\r\n  {\r\n    \"ParticipantPeopleImage\": \"https://picsum.photos/200/200/?random=18\",\r\n    \"ParticipantPeopleId\": \"FFFF\"\r\n  }\r\n]\r\n",
+                  EventImage = "https://picsum.photos/1300/600/?random=15",
+                  Introduction = "實體活動簡介內容區",
+                  Description = "實體活動描述內容區",
+                  MainOrganizer = "卡米地",
+                  IsPrivateEvent = false,
+                  IsFree = false,
+                  Status = 1,
+                  CreatedAt = DateTime.Now,
+                  EditedAt = DateTime.Now
+              }
+           );
+
+
+            modelBuilder.Entity<CategoryTag>().HasData(
+                new CategoryTag
+                {
+                    Id= 1,
+                    Name= "音樂",
+                    CreatedAt = DateTime.Now,
+                    EditeAt = DateTime.Now,
+                },
+                new CategoryTag
+                {
+                    Id = 2,
+                    Name = "戲劇",
+                    CreatedAt = DateTime.Now,
+                    EditeAt = DateTime.Now,
+                },
+                new CategoryTag
+                {
+                    Id = 3,
+                    Name = "展覽",
+                    CreatedAt = DateTime.Now,
+                    EditeAt = DateTime.Now,
+                }
+            );
+
+
+
         }
     }
 }
