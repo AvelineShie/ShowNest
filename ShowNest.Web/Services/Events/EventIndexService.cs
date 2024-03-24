099abafc4b50d0ded6f6469fa0b21525afef4976
Song <mantourm@gmail.com>
2024-03-24 21:27:06 +0800

    Fix EventIndexService

--- ShowNest.Web/Services/Events/EventIndexService.cs
+++ ShowNest.Web/Services/Events/EventIndexService.cs
@@ -7,6 +7,12 @@ namespace ShowNest.Web.Services.Events
         private readonly CategoryTagService _categoryTagService;
         private readonly EventCardService _eventCardService;
 
+        public EventIndexService(CategoryTagService categoryTagService, EventCardService eventCardService)
+        {
+            _categoryTagService = categoryTagService;
+            _eventCardService = eventCardService;
+        }
+
         public EventIndexViewModel GetEventIndexViewModel()
         {
             return new EventIndexViewModel
