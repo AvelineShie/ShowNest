7e9c062063abafed1df28afb255e3c3ba4074363
Aveline <evelyne1122@gmail.com>
2024-03-26 11:53:41 +0800

    註解Event Route

--- ShowNest.Web/Program.cs
+++ ShowNest.Web/Program.cs
@@ -63,10 +63,10 @@ namespace ShowNest.Web
             //    pattern: "{OrganizationId}/{controller=Events}/{action=Index}/{EventId?}");
             ///以上測試中--------------------------------------------------------------------------------------------
 
-            app.MapControllerRoute(
-                name: "eventPages",
-                pattern: "Events/{page=1}",
-            defaults: new { controller = "Events", action = "Index"});
+            //app.MapControllerRoute(
+            //    name: "eventPages",
+            //    pattern: "Events/{page=1}",
+            //defaults: new { controller = "Events", action = "Index"});
 
             app.MapControllerRoute(
             name: "NewEvent",
