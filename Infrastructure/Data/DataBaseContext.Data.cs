357bd0b49241240e63debb6ec019dc8b9223d239
Song <mantourm@gmail.com>
2024-03-27 20:56:31 +0800

    Update merge things

--- Infrastructure/Data/DataBaseContext.Data.cs
+++ Infrastructure/Data/DataBaseContext.Data.cs
@@ -7,20 +7,20 @@ namespace Infrastructure.Data
     {
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Area>().HasData(
-             new Area { Id = 1, Name = "北北基宜地區" },
-             new Area { Id = 2, Name = "桃竹苗地區" },
-             new Area { Id = 3, Name = "雲嘉南地區" },
-             new Area { Id = 4, Name = "高屏地區" },
-             new Area { Id = 5, Name = "中彰投地區" },
-             new Area { Id = 6, Name = "花東地區" },
-             new Area { Id = 7, Name = "澎金馬地區" },
-             new Area { Id = 8, Name = "香港" },
-             new Area { Id = 9, Name = "澳門" },
-             new Area { Id = 10, Name = "其他地區" }
-             );
+            //modelBuilder.Entity<Area>().HasData(
+            // new Area { Id = 1, Name = "北北基宜地區" },
+            // new Area { Id = 2, Name = "桃竹苗地區" },
+            // new Area { Id = 3, Name = "雲嘉南地區" },
+            // new Area { Id = 4, Name = "高屏地區" },
+            // new Area { Id = 5, Name = "中彰投地區" },
+            // new Area { Id = 6, Name = "花東地區" },
+            // new Area { Id = 7, Name = "澎金馬地區" },
+            // new Area { Id = 8, Name = "香港" },
+            // new Area { Id = 9, Name = "澳門" },
+            // new Area { Id = 10, Name = "其他地區" }
+            // );
 
-            FakeDataFilling(modelBuilder);
+            //FakeDataFilling(modelBuilder);
         }
     }
 }
