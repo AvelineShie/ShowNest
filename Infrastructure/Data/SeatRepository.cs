7a951b7cff0a8cad80a269e21582dfc8dff68350
Liling Cheng <libclf1026@gmail.com>
2024-04-02 23:30:39 +0800

    update SeatService.cs

--- Infrastructure/Data/SeatRepository.cs
+++ Infrastructure/Data/SeatRepository.cs
@@ -5,19 +5,19 @@ using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Infrastructure.Data;
 
-public class SeatRepository: EfRepository<Seat>, ISeatRepository
-{
-
-    public SeatRepository(DatabaseContext context) : base(context)
-    {
-    }
-    
-    public async Task<List<Seat>> GetSeatsBySeatAreaId(int seatAreaID)
-    {
-        var seats = await this.DbContext.Seats
-            .Where(i => i.SeatAreaId == seatAreaID)
-            .ToListAsync();
-
-        return seats;
-    }
-}
+// public class SeatRepository: EfRepository<Seat>, ISeatRepository
+// {
+//
+//     public SeatRepository(DatabaseContext context) : base(context)
+//     {
+//     }
+//     
+//     public async Task<List<Seat>> GetSeatsBySeatAreaId(int seatAreaID)
+//     {
+//         var seats = await this.DbContext.Seats
+//             .Where(i => i.SeatAreaId == seatAreaID)
+//             .ToListAsync();
+//
+//         return seats;
+//     }
+// }
