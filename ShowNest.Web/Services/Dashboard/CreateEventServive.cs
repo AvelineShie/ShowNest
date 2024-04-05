6b08bfb92027cbb6dc180a18be00b7cde6452b76
Aveline <evelyne1122@gmail.com>
2024-04-05 18:41:16 +0800

    correct build

--- ShowNest.Web/Services/Dashboard/CreateEventServive.cs
+++ ShowNest.Web/Services/Dashboard/CreateEventServive.cs
@@ -18,22 +18,19 @@ namespace ShowNest.Web.Services.Dashboard
         }
 
         //1. 撈擁有人底下的所有OrgId, OrgName組成VM
-        public CreateEventViewModel GetOrgByOwner(int OwnerId)
-        {
-            var orgNames = _eventRepository.GetOrgIdByOwnerId(OwnerId);
-            var evetNames = _eventRepository.GetEventIdByOrgId(OrgId);
+        //public CreateEventViewModel GetOrgByOwner(int OwnerId)
+        //{
+        //    var orgNames = _eventRepository.GetOrgIdByOwnerId(OwnerId);
+        //    var evetNames = _eventRepository.GetEventIdByOrgId(OrgId);
 
-            foreach (OrgName in orgNames) 
-            {
-                var result = new CreateEventViewModel
-                {
-                    OrgNames = 
-                }
-
-            }
-            
-
-        }
+        //    foreach (OrgName in orgNames)
+        //    {
+        //        var result = new CreateEventViewModel
+        //        {
+        //            OrgNames =
+        //        }
+        //    }
+        //}
 
         //2.撈組織下面所有活動，組成VM
         //public IEnumerable<Event> GetEventIdByOrgId(int orgId)
