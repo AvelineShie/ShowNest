1d8d9bb11e7d71be30dad37df86b1cf581420705
Liling Cheng <libclf1026@gmail.com>
2024-04-06 23:51:46 +0800

    Refactor to option api

--- ShowNest.Web/wwwroot/js/Events/TicketTypeSelectionVue.js
+++ ShowNest.Web/wwwroot/js/Events/TicketTypeSelectionVue.js
@@ -1,51 +1,44 @@
-const {createApp, onMounted, ref} = Vue
+const {createApp} = Vue
 
 createApp({
-    setup() {
-        const ticketTypeSelection = ref({})
-        const isAgreed = ref(false)
-
-        async function fetchTicketTypes() {
+    data() {
+        return {
+            ticketTypeSelection: {},
+            isAgreed: false
+        }
+    },
+    methods: {
+        async fetchTicketTypes() {
             const response = await fetch('/api/tickettypes/gettickettypeselection?eventId=100');
             const data = await response.json()
 
-            ticketTypeSelection.value = data
-        }
-
-        function onTicketPriceRowClicked(ticketPriceRow, num) {
+            this.ticketTypeSelection = data
+        },
+        onTicketPriceRowClicked(ticketPriceRow, num) {
             if (ticketPriceRow.tickets.purchaseAmount + num < 0) {
                 return
             }
 
             ticketPriceRow.tickets.purchaseAmount += num
-        }
-
-        function onNextStepClicked() {
-            if (!isAgreed.value) {
+        },
+        onNextStepClicked() {
+            if (!this.isAgreed) {
                 alert('Not agreed');
                 return;
             }
 
             const params = new URLSearchParams();
-            for (const ticketPriceRow of ticketTypeSelection.value.ticketPriceRow) {
+            for (const ticketPriceRow of this.ticketTypeSelection.ticketPriceRow) {
                 if (ticketPriceRow.tickets.purchaseAmount !== 0) {
                     params.append(ticketPriceRow.id, ticketPriceRow.tickets.purchaseAmount);
                 }
             }
-          
+
             const redirectUrl = `/events/seatSelector?${params.toString()}`
             window.location = redirectUrl;
-        }
-
-        onMounted(async () => {
-            await fetchTicketTypes();
-        })
-
-        return {
-            isAgreed,
-            ticketTypeSelection,
-            onNextStepClicked,
-            onTicketPriceRowClicked
-        }
+        },
+    },
+    async mounted() {
+        await this.fetchTicketTypes();
     }
 }).mount('#app')
