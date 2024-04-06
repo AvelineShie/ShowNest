2d2a2e0876c97f257ae43a5b3149b6f9085908e1
Dani <1073B023@gmail.com>
2024-04-06 16:04:19 +0800

    feat: Update OrderApi and CompStep JS, and OrderDetail Vue.js content for dynamic rendering from database

--- ShowNest.Web/WebAPI/OrdersController.cs
+++ ShowNest.Web/WebAPI/OrdersController.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using static Infrastructure.Services.OrderAPIService;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,19 +31,40 @@ namespace ShowNest.Web.WebAPI
             return Ok(_orderAPIService.GetOrderDetailByOrderId(userId,orderId));
         }
 
+        //[HttpPost]
+        //public async Task<IActionResult> UpdateOrderDetail (int orderId, string contactPersonJson)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+        //    var order = await _orderTicketService.UpdateOrderDetailContactInfoByOrderId(contactPersonJson,orderId);
+        //    if (order is null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(order);
+        //}
         [HttpPost]
-        public async Task<IActionResult> UpdateOrderDetail (int orderId, string contactPersonJson)
+        public async Task<IActionResult> UpdateOrderDetail(OrderContactPerson param)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            var order = await _orderTicketService.UpdateOrderDetailContactInfoByOrderId(contactPersonJson,orderId);
-            if (order is null)
+            int orderId = param.OrderID;
+            string contactPersonJson = param.ContactPersonJson;
+            var order = await _orderTicketService.UpdateOrderDetailContactInfoByOrderId(contactPersonJson, orderId);
+            try
             {
-                return NotFound();
+                //return Ok(_orderAPIService.UpdateOrderContactPerson(contactPersonJson, orderId));
+                return Ok(order);
             }
-            return Ok(order);
+            catch (Exception ex)
+            {
+                return Ok(ex);
+            }
+
         }
     }
 }
