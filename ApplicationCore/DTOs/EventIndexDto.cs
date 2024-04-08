e0c1dcddc2a173cfd999988047ffad6d8cce595f
Song <mantourm@gmail.com>
2024-04-09 00:01:52 +0800

    temp update events index for api solution (card appending done, pagination ongoing)

--- ApplicationCore/DTOs/EventIndexDto.cs
+++ ApplicationCore/DTOs/EventIndexDto.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ApplicationCore.DTOs
 {
     public class EventIndexDto
     {
-            public string EventId { get; set; }
-            public string EventName { get; set; }
-            public string EventImgUrl { get; set; }
-            public string CategoryName { get; set; }
-            public DateTime EventTime { get; set; }
+        public string EventId { get; set; }
+        public string EventName { get; set; }
+        public string EventImgUrl { get; set; }
+        public string CategoryName { get; set; }
+        public DateTime EventTime { get; set; }
+        public string EventStatus { get; set; }
+        public string EventStatusCssClass { get; set; }
     }
 }
--- Infrastructure/Services/EventsIndexCardsAPIServiceByEf.cs
+++ Infrastructure/Services/EventsIndexCardsAPIServiceByEf.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore;
 using ApplicationCore.DTOs;
+using ApplicationCore.Entities;
 using ApplicationCore.Helpers;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,25 @@ namespace Infrastructure.Services
             _databaseContext = databaseContext;
         }
 
+        static List<string> GetEventStatusAndCssClassName(DateTime input)
+        {
+            DateTime curr = DateTime.Now;
+
+            if (curr < input)
+            {
+                return new List<string> { "開賣中", "green-status" };
+            }
+            else
+            {
+                return new List<string> { "已結束", "black-status" };
+            }
+        }
+
+        //static string ConvertEventTime(DateTime input)
+        //{
+
+        //}
+
         public async Task<OperationResult> GetCardsByPagesize(int page, int cardsPerPage)
         {
             try
@@ -27,9 +47,9 @@ namespace Infrastructure.Services
                 var cardsForCurrerntPage = await _databaseContext.EventAndTagMappings
                 .Include(et => et.Event)
                 .Include(et => et.CategoryTag)
-                .OrderBy(et => et.CategoryTagId)
+                .OrderByDescending(et => et.CategoryTagId)
                 .ThenBy(et => et.EventId)
-                .Skip(page - 1).Take(cardsPerPage)
+                .Skip((page - 1) * 9).Take(cardsPerPage)
                 .Select(et => new EventIndexDto
                 {
                     EventId = et.Event.Id.ToString(),
@@ -37,6 +57,8 @@ namespace Infrastructure.Services
                     EventImgUrl = et.Event.EventImage,
                     CategoryName = et.CategoryTag.Name,
                     EventTime = et.Event.StartTime,
+                    EventStatus = GetEventStatusAndCssClassName(et.Event.StartTime)[0],
+                    EventStatusCssClass = GetEventStatusAndCssClassName(et.Event.StartTime)[1],
                 })
                 .ToListAsync();
 
@@ -47,5 +69,33 @@ namespace Infrastructure.Services
                 return OperationResultHelper.ReturnErrorMsg(ex.Message);
             }
         }
+
+        //public async Task<OperationResult> GetCardsByPagesize()
+        //{
+        //    try
+        //    {
+        //        var cardsForCurrerntPage = await _databaseContext.EventAndTagMappings
+        //        .Include(et => et.Event)
+        //        .Include(et => et.CategoryTag)
+        //        .OrderBy(et => et.CategoryTagId)
+        //        .ThenBy(et => et.EventId)
+        //        .Skip(1).Take(9)
+        //        .Select(et => new EventIndexDto
+        //        {
+        //            EventId = et.Event.Id.ToString(),
+        //            EventName = et.Event.Name,
+        //            EventImgUrl = et.Event.EventImage,
+        //            CategoryName = et.CategoryTag.Name,
+        //            EventTime = et.Event.StartTime,
+        //        })
+        //        .ToListAsync();
+
+        //        return OperationResultHelper.ReturnSuccessData(cardsForCurrerntPage);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return OperationResultHelper.ReturnErrorMsg(ex.Message);
+        //    }
+        //}
     }
 }
