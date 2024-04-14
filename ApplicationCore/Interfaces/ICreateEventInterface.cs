88c83195299c8e4244d02a3671c3f4e73f6f9947
Aveline <evelyne1122@gmail.com>
2024-04-15 06:55:27 +0800

    add interface

---
+++ ApplicationCore/Interfaces/ICreateEventInterface.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.DTOs;
+using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    internal interface ICreateEventInterface: IRepository<Event>
+    {
+        IEnumerable<Organization> GetOrgById(int userId);
+        IEnumerable<Event> GetOrgEventsByOrgId(int orgId);
+        public Event CreateEvent(CreateEventDto require);
+    }
+}
