e866b0fab192f3f021fa3decac0ab3cdf6e6359b
Aveline <evelyne1122@gmail.com>
2024-04-15 07:10:17 +0800

    改錯了改回去, 抱歉！

---
+++ ShowNest.Web/WebAPI/CreateEventController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShowNest.Web.ViewModels.EventsApiDtos;
+using ShowNest.Web.ViewModels.Shared;
+using System.Security.Claims;
+
+namespace ShowNest.Web.WebAPI
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CreateEventController : ControllerBase
+    {
+        
+
+    }
+}
--- ShowNest.Web/WebAPI/EventsController.cs
+++ ShowNest.Web/WebAPI/EventsController.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-using ShowNest.Web.ViewModels.Shared;
-using Microsoft.EntityFrameworkCore;
 using ShowNest.Web.ViewModels.EventsApiDtos;
 using Organization = ShowNest.Web.ViewModels.EventsApiDtos.Organization;
-using Microsoft.AspNetCore.Http;
-using System.Security.Claims;
 
 namespace ShowNest.Web.WebAPI;
 
@@ -13,50 +9,41 @@ namespace ShowNest.Web.WebAPI;
 public class EventsController : ControllerBase
 {
     private readonly EventsApiService _eventsApiService;
-    private readonly DatabaseContext _context;
-    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public EventsController(EventsApiService eventsApiService, DatabaseContext context, IHttpContextAccessor httpContextAccessor)
+    public EventsController(EventsApiService eventsApiService)
     {
         _eventsApiService = eventsApiService;
-        _context = context;
-        _httpContextAccessor = httpContextAccessor;
     }
-
-    
-
 }
-
 
 //[HttpPost]
 //public IActionResult GetOrganizationsById([FromBody] GetOrganizationRequest request)
 //{
-
-//if (!ModelState.IsValid)
-//{
-//    return BadRequest(new
+//    if (!ModelState.IsValid)
 //    {
-//        IsSuccess = false,
-//        Message = "Invalid request"
-//    });
+//        return BadRequest(new
+//        {
+//            IsSuccess = false,
+//            Message = "Invalid request"
+//        });
+//    }
+
+//    var organizations = _eventsApiService.GetOrganizationsById(request.UserId);
+
+//    if ((organizations.Count <= 0)
+//        {
+//        return Ok(new { IsSuccess = false, Message = "No organization found" });
+//    }
+
+//    var result = new GetOrganizationResponse()
+//    {
+//        Organizations = organizations.Select(x => new Organization()
+//        {
+//            Id = x.Id,
+//            Name = x.Name
+//        }).ToList()
+//    };
+
+//    return Ok(new { IsSuccess = true, Body = result });
 //}
-
-//var organizations = _eventsApiService.GetOrganizationsById(request.UserId);
-
-//if ((organizations.Count <= 0)
-//    {
-//    return Ok(new { IsSuccess = false, Message = "No organization found" });
-//}
-
-//var result = new GetOrganizationResponse()
-//{
-//    Organizations = organizations.Select(x => new Organization()
-//    {
-//        Id = x.Id,
-//        Name = x.Name
-//    }).ToList()
-//};
-
-//return Ok(new { IsSuccess = true, Body = result });
-
 //}
