f4103dfc06f5e4ce696a178729352ca7e5d24de9
Dani <1073B023@gmail.com>
2024-04-16 19:00:09 +0800

    Update Ecpay integration process and fix Order Render issue and CSS

--- ShowNest.Web/WebAPI/EcpayController.cs
+++ ShowNest.Web/WebAPI/EcpayController.cs
@@ -27,6 +27,7 @@ namespace ShowNest.Web.WebAPI
             _ecpayOrderService = ecpayOrderService;
         }
 
+      //-----------------------------------原始粗暴版綠界新增訂單在DB------------------------------------------------
         [HttpPost]
         [Route("api/Ecpay/AddOrders")]
         public string AddOrders([FromBody] OrderDto json)
@@ -84,7 +85,6 @@ namespace ShowNest.Web.WebAPI
         [HttpPost]
         [Route("api/Ecpay/AddPayInfo")]
         public HttpResponseMessage AddPayInfo(IFormCollection info ,string id)
-        public HttpResponseMessage AddPayInfo(IFormCollection info)
         {
             try
             {
