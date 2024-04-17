43f8a110d0407ad64fdeb421a1f290a24ff4aa2e
PJ shi <michelle760994@gmail.com>
2024-04-17 20:09:31 +0800

    Fix：Organizations/Index渲染

--- ShowNest.Web/Services/Organizations/OrganizationIndexService.cs
+++ ShowNest.Web/Services/Organizations/OrganizationIndexService.cs
@@ -16,49 +16,36 @@ namespace ShowNest.Web.Services.Organization
         public OrganizationIndexViewModel GetOrganizationDetails(int organizationId)
         {
             var organization = _databaseContext.Organizations
-                                .Include(o => o.Events)
-                                .FirstOrDefault(o => o.Id == organizationId);
+                        .Include(o => o.Events)
+                        .FirstOrDefault(o => o.Id == organizationId);
 
             if (organization == null)
             {
                 return null;
             }
-            var currentDate = DateTime.Now.Date;
+            var currentDate = DateTime.Now; 
 
-            // 先將事件按照開始時間排序
-            var sortedEvents = organization.Events.OrderBy(e => e.StartTime);
 
-            // 將事件按照開始時間的月份分組
-            var groupedEvents = sortedEvents
+
+            // 從符合organizationId的活動事件中 取得所有現正進行中的活動事件
+            var currentEvents = organization.Events
+                .Where(e => e.EndTime >= currentDate)
+                .OrderBy(e => e.StartTime)
+                .ToList();
+
+            // 從符合organizationId的活動事件中 取得結束的活動事件
+            var pastEvents = organization.Events
+                .Where(e => e.EndTime < currentDate)
+                .ToList();
+
+            
+
+            // 將已經結束的事件按照開始時間的月份分組
+            var pastGroupedEvents = pastEvents
                 .GroupBy(e => e.StartTime.Month)
                 .Select(g => new { Month = g.Key, Events = g.ToList() })
                 .ToList();
 
-            var currentEvents = groupedEvents.FirstOrDefault(g => g.Month == currentDate.Month)?.Events
-                .Select(e => new EventDetail
-                {
-                    Id = e.Id,
-                    EventImage = e.EventImage,
-                    EventName = e.Name,
-                    StartTime = e.StartTime,
-                    EventIntroduction = e.Introduction,
-                }).ToList();
-
-            // 將過去的事件按照月份分類
-            var pastEventsGroupedByMonth = groupedEvents
-                .Where(g => g.Month < currentDate.Month)
-                .Select(g => new {
-                    g.Month,
-                    Events = g.Events.Select(e => new EventDetail
-                    {
-                Id = e.Id,
-                EventImage = e.EventImage,
-                EventName = e.Name,
-                StartTime = e.StartTime,
-                EventIntroduction = e.Introduction,
-            }).ToList()
-        })
-        .ToList();
             var result = new OrganizationIndexViewModel
             {
                 OrganizationId = organization.Id,
@@ -68,11 +55,27 @@ namespace ShowNest.Web.Services.Organization
                 OrganizationWeb = organization.OrganizationUrl,
                 OrganizationFBLink = organization.Fblink,
                 OrganizationEmail = organization.Email,
-                CurrentEvents = currentEvents,
-                GroupedPastEvents = pastEventsGroupedByMonth.Select(g => new EventDetail
+                GroupedCurrentEvents = currentEvents
+                .Select(e => new EventDetail
                 {
-                    Month = g.Month
-                   
+                    Id = e.Id,
+                    EventImage = e.EventImage,
+                    EventName = e.Name,
+                    StartTime = e.StartTime,
+                    EventIntroduction = e.Introduction,
+                })
+                .ToList(),
+                GroupedPastEvents = pastGroupedEvents.Select(g => new EventDetail
+                {
+                    Month = g.Month,
+                    AllEvents = g.Events.Select(e => new EventDetail
+                    {
+                        Id = e.Id,
+                        EventImage = e.EventImage,
+                        EventName = e.Name,
+                        StartTime = e.StartTime,
+                        EventIntroduction = e.Introduction,
+                    }).ToList()
                 }).ToList()
             };
 
