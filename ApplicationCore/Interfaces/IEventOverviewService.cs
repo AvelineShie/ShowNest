31ed06681f7304f3c2efab8c3b2c4b02e04ba76c
Song <mantourm@gmail.com>
2024-04-23 12:35:49 +0800

    temp update Event dashboard

---
+++ ApplicationCore/Interfaces/IEventOverviewService.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IEventOverviewService
+    {
+        Task<List<EventsOverviewTicketsDto>> GetEventsOverviewTicketInfo(int eventId);
+    }
+}
