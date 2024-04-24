d66a3cf491f168c957258acc9c5985db6f69c89e
Song <mantourm@gmail.com>
2024-04-24 21:54:59 +0800

    update events overview page

--- ApplicationCore/DTOs/EventsOverviewDto.cs
+++ ApplicationCore/DTOs/EventsOverviewDto.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.DTOs
 {
-    public class EventsOverviewTicketsDto
+    public class EventsOverviewDto
     {
         public int EventId { get; set; }
+        public string EventName { get; set; }
+        public int AllSoldTicketsCount { get; set; }
+        public int AllRemainedTicketsCount { get; set; }
+        public int AllTicketsAmount { get; set; }
+        public string EventTime { get; set; }
+        public string EventPlace { get; set; }
+        public List<EventsOverviewTicketsDto>? Tickets { get; set; }
+        public List<EventsOverviewOrdersDto>? Orders { get; set; }
+    }
+    public class EventsOverviewTicketsDto
+    {
         public int TicketTypeId { get; set; }
         public string TicketTypeName { get; set; }
         public string StartSellingTime { get; set; }
@@ -16,9 +27,19 @@ namespace ApplicationCore.DTOs
         public bool SellingStatus { get; set; }
         public int Price { get; set; }
         public int SoldAmount { get; set;}
-        public int PaidAmout { get; set; }
-        public int WaitingToPayAmout { get; set; }
-        public int RemainAmout { get; set; }
+        public int PaidAmount { get; set; }
+        public int WaitingToPayAmount { get; set; }
+        public int RemainAmount { get; set; }
         public int PriceOfPaidAmout { get; set; }
+    }
+
+    public class EventsOverviewOrdersDto
+    {
+        public int OrderId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhone { get; set; }
+        public int OrderedTicketAmount { get; set; }
+        public int OrderedTicketTotalPrice { get; set; }
     }
 }
